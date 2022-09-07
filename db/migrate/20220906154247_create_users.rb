eff25e0a4401cf3176c71ca29c73942f228ebe12
Kweeka <mtalbi29@yahoo.com>
2022-09-07 23:50:33 +0100

    added user model

--- db/migrate/20220906154247_create_users.rb
+++ db/migrate/20220906154247_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users, :id => false, :primary_key  => :id do |t|
-      t.string :id, null: false
+    create_table :users do |t|
+      t.string :user_guid, null: false
       t.string :user, null: false, index: { unique: true }
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_06_154247) do
-  create_table "users", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "id", null: false
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "user_guid", null: false
     t.string "user", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
