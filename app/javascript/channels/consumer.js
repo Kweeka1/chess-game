fc03d33d851ba57ccd91de253febba98d595589d
Kweeka <mtalbi29@yahoo.com>
2022-09-07 23:54:44 +0100

    refactor

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -5,5 +5,6 @@ import { createConsumer } from "@rails/actioncable"
 
 const id = window.location.pathname.split("/").at(-1)
 
-export default createConsumer(`ws://192.168.1.15:3000/cable?board=${id}`)
+export const createChessConsumer = createConsumer(`ws://192.168.1.15:3000/cable?room=${id}`)
+export const createLobbyConsumer = createConsumer(`ws://192.168.1.15:3000/cable?room=global`)
 //export default createConsumer()
