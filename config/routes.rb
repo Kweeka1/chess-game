b2d6e911313619a346ddd432d5cad3b956aa91d7
Kweeka <mtalbi29@yahoo.com>
2022-09-10 15:40:04 +0100

    removed /chess route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/chess/:id', to: "chess#index"
-  get '/chess/', to: 'chess#get_board'
   get '/', to: 'lobby#index'
+  post 'create-room', to: 'lobby#create_room'
   post '/username', to: 'lobby#change_username'
   # Defines the root path route ("/")
   root "lobby#index"
