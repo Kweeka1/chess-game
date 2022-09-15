5580b7db1569d1c57eeec20b370fa4502fed9e21
Kweeka <mtalbi29@yahoo.com>
2022-09-15 16:21:28 +0100

    added user channel

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -4,7 +4,9 @@
 import { createConsumer } from "@rails/actioncable"
 
 const id = window.location.pathname.split("/").at(-1)
+export const username = document.getElementById("username").value
 
 export const createChessConsumer = createConsumer(`ws://192.168.1.15:3000/cable?room=${id}`)
 export const createLobbyConsumer = createConsumer(`ws://192.168.1.15:3000/cable?room=global`)
+export const createUserConsumer = createConsumer(`ws://192.168.1.15:3000/cable?room=global&user=${username}`)
 //export default createConsumer()
