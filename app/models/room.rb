b3a7cc9cddce5e8baaf2be7b3c200fd0752d5c5e
Kweeka <mtalbi29@yahoo.com>
2022-09-15 16:27:53 +0100

    added validation for room password

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   validates :room_id, uniqueness: true
   validates :room_name, uniqueness: true, length: { maximum: 30, minimum: 5 }
+  validates :room_password, allow_blank: true, length: { maximum: 24, minimum: 6 }
   validates :room_description, length: { maximum: 105 }
   validates :room_allow_viewers, inclusion: {in: %w(allow permission no)}
   validates :room_only_players_chat, inclusion: {in: %w(players all)}
