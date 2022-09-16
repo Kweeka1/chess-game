17b101b19616884fd90f7478abe284170c260d8e
Kweeka <mtalbi29@yahoo.com>
2022-09-16 11:02:11 +0100

    added join/view request and response between room host and guest.

--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -19,9 +19,9 @@ class UserChannel < ApplicationCable::Channel
   def receive(request)
     case request["type"]
       when @@message_types[:join_request]
-        then handle_join_request(request["data"]["guest"], request["data"]["host"])
-      when @@message_types[:join_request]
-        then handle_view_request(request["data"]["guest"], request["data"]["host"])
+        then handle_join_request(request["data"]["guest"], request["data"]["host"], request["data"]["room"])
+      when @@message_types[:view_request]
+        then handle_view_request(request["data"]["guest"], request["data"]["host"], request["data"]["room"])
     end
   end
 
@@ -40,11 +40,11 @@ class UserChannel < ApplicationCable::Channel
     ActionCable.server.broadcast "user_channel_#{params[:user]}", {type: @@message_types[:current_rooms], data: rooms}
   end
 
-  def handle_join_request(guest, host)
-    ActionCable.server.broadcast "user_channel_#{host}", { type: @@message_types[:join_request], data: { guest: guest }}
+  def handle_join_request(guest, host, room)
+    ActionCable.server.broadcast "user_channel_#{host}", { type: @@message_types[:join_request], data: { guest: guest, room: room }}
   end
 
-  def handle_view_request(guest, host)
-    ActionCable.server.broadcast "user_channel_#{host}", { type: @@message_types[:join_request], data: { guest: guest }}
+  def handle_view_request(guest, host, room)
+    ActionCable.server.broadcast "user_channel_#{host}", { type: @@message_types[:view_request], data: { guest: guest, room: room }}
   end
 end
